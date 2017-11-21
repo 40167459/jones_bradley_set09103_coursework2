35a6579daeca6cd5cfacb3e1fafe8bd779295028
40167459 <jonesbradley12@yahoo.co.uk>
2017-11-21 15:17:34 +0000

    Continued to work on the templates

--- sourcecode/home.py
+++ sourcecode/home.py
@@ -6,6 +6,9 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/home') #Redirects to homepage
+def home():
+  return render_template('home.html') #Renders the appropriate templates
 
 @app.errorhandler(404) #Error handing, redirects to error screen
 def page_not_found(error):
