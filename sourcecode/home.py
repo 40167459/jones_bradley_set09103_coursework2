b834e5b38cca3956514d4cd372555770b5b736e9
40167459 <jonesbradley12@yahoo.co.uk>
2017-11-21 13:47:57 +0000

    First commit, added error handling to the homepage.

---
+++ sourcecode/home.py
@@ -0,0 +1,15 @@
+#SET09103 CW2
+#40167459
+#Bradley Jones
+#20/11/2017
+#Imports from the flask
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.errorhandler(404) #Error handing, redirects to error screen
+def page_not_found(error):
+  return "Couldn't find the page you requested.", 404
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
